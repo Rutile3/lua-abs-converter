d0d430b79c819b1878dd7a3ca925c0eea6890fc2
Rutile3 <29337073+Rutile3@users.noreply.github.com>
2026-06-04 13:06:11 +0900

    【機能追加】サンプルボタンの処理を実装。

---
+++ app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(() => {
+    const sourceCode = document.getElementById('source-code');
+    const convertedCode = document.getElementById('converted-code');
+
+    const loadSampleButton = document.getElementById('load-sample-button');
+    const clearSourceButton = document.getElementById('clear-source-button');
+    const copyConvertedButton = document.getElementById('copy-converted-button');
+
+    loadSampleButton.addEventListener('click', () => {
+        sourceCode.value = [
+            'if abs(x) <= 3 and y == 1 then',
+            '  if abs(x) == 4 and z == 1 then return true end',
+            'end',
+            'y = abs(x) == N',
+            'z = abs(a) <= (A+B)',
+            'w = abs(foo) == 3.5',
+        ].join('\n');
+    });
+})();
